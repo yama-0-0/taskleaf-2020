549a9da037fc4850e3da26a60240d03811c2d283
taka president <life.i.s.w---@i.softbank.jp>
2020-02-13 11:39:45 +0900

    新規作成の確認画面実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,16 +9,22 @@ class TasksController < ApplicationController
   end
 
   def new
-   @task = Task.new
+    @task = Task.new
   end
 
   def create
-   @task = current_user.tasks.new(task_params)
-   if @task.save
-     redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
-   else
-     render :new
-   end
+    @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
+
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -32,6 +38,11 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
   end
 
   private
